2c685e419cf68bebd0ded41fbd60343e0f52d8db
leon <leon@your.heart>
2019-05-02 16:56:58 +0800

    first commit.

---
+++ code/main.lua
@@ -0,0 +1,9 @@
+local class = require "class-clean"
+local Window = class("Window", {width = 150, height = 100})
+
+print (Window.name)
+
+local appWindow = Window()
+print (appWindow.width, appWindow.height)
+
+require("notsogood-scenemanager")
---
+++ code/notsogood-scenemanager.lua
@@ -0,0 +1,29 @@
+local class = require "class-clean"
+local SceneManager = class("SceneManager", {_m_state = "開始"})
+print (SceneManager.name .. " class ready.")
+
+function SceneManager:ChangeScene(stateName)
+	local switch = {
+		[1] = function()	-- for case 1
+			print "Case 1."
+		end,
+		[2] = function()	-- for case 2
+			print "Case 2."
+		end,
+		["開始"] = function()	-- for case 3
+			print "Case 3 開始."
+		end
+	}
+
+	-- local switch_result = switch[self._m_state]
+	local switch_result = switch[stateName]
+
+	if (switch_result) then switch_result() else print "Case default (not found)." end
+end
+
+
+local sceneManager = SceneManager()
+--print (appWindow.width, appWindow.height)
+sceneManager:ChangeScene(2) --"case 2"
+sceneManager:ChangeScene(8888) --default not found
+sceneManager:ChangeScene("開始") --"case 3 開始"
